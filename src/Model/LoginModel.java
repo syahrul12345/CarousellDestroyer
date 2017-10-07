44f97bed94332ca04ed66670c7a4824470b6c098
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2017-10-07 10:13:28 +0800

    Added captcha pull and solve capability with anti-captha api

--- src/Model/LoginModel.java
+++ src/Model/LoginModel.java
@@ -1,11 +1,13 @@
 package Model;
 
-import com.anti_captcha.AnticaptchaBase;
-import javafx.collections.ObservableList;
+import com.anti_captcha.Api.ImageToText;
+import com.anti_captcha.Api.NoCaptchaProxyless;
+import com.anti_captcha.Helper.DebugHelper;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Observable;
 import java.util.concurrent.Callable;
 
@@ -21,9 +23,10 @@ public class LoginModel extends Observable implements Callable {
 
     }
 
+
+
     @Override
     public Object call() throws Exception {
-
         //fills up the login info
 
         HtmlUnitDriver htmlUnitDriver = new HtmlUnitDriver();
@@ -31,8 +34,8 @@ public class LoginModel extends Observable implements Callable {
         htmlUnitDriver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys(user);
         htmlUnitDriver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys(password);
 
-
-
+        AntiCaptcha();
+        GetBalance();
 
         if(htmlUnitDriver.getTitle().contains("Login")) {
             loginFlag = true;
@@ -42,6 +45,43 @@ public class LoginModel extends Observable implements Callable {
         setChanged();
         notifyObservers();
         return loginFlag;
+    }
+    //solves the captcha and returns the hash
+    private void AntiCaptcha() throws InterruptedException, MalformedURLException {
+        DebugHelper.setVerboseMode(true);
+        NoCaptchaProxyless api = new NoCaptchaProxyless();
+        api.setClientKey("be43df9db92c4a3c381683be8db91e26");
+        api.setWebsiteUrl(new URL("https://sg.carousell.com"));
+        api.setWebsiteKey("6Lc-KiYTAAAAAGkGKh1LNFmCAlEGvhABCl4o56SE");
+
+        if (!api.createTask()) {
+            DebugHelper.out(
+                    "API v2 send failed. " + api.getErrorMessage(),
+                    DebugHelper.Type.ERROR
+            );
+        } else if (!api.waitForResult()) {
+            DebugHelper.out("Could not solve the captcha.", DebugHelper.Type.ERROR);
+        } else {
+            DebugHelper.out("Result: " + api.getTaskSolution(), DebugHelper.Type.SUCCESS);
+        }
+    }
+    private void GetBalance()
+    {
+        DebugHelper.setVerboseMode(true);
+
+        ImageToText api = new ImageToText();
+        api.setClientKey("be43df9db92c4a3c381683be8db91e26");
+
+        Double balance = api.getBalance();
+
+        if (balance == null)
+        {
+            DebugHelper.out("GetBalance() failed. " + api.getErrorMessage(), DebugHelper.Type.ERROR);
+        }
+        else
+        {
+            DebugHelper.out("Balance: " + balance, DebugHelper.Type.SUCCESS);
+        }
 
     }
 
